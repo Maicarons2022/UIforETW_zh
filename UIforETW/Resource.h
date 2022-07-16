0a1bc4d89f1eb9b82db04e4c1ac4f3a9131a3491
EldersJavas <EldersJavas@gmail.com>
2022-07-16 09:36:40 +0800

    update zh_CN

--- UIforETW/Resource.h
+++ UIforETW/Resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by UIforETW.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 zh_CN.rc 使用
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
